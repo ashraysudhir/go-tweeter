23b78fa280668ceb9f057a6b50b5a7d00533a10b
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 02:27:47 +0530

    Modified the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20170722203621) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password"
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
