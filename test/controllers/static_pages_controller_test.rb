9ce1d7ce9b6ba0b772a31330b44b76e440a21058
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-22 23:11:23 +0530

    Updated the routes to incorporate _path convention

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,25 +7,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@title_root}"
   end
 
   test "should get contact us" do
-    get static_pages_contactus_url
+    get contactus_path
     assert_response :success
     assert_select "title", "Contact Us | #{@title_root}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@title_root}"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
 end
