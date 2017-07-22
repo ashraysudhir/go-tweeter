5d46574aba71aa264781002ed980e30f8cb1b74e
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 00:21:57 +0530

    Added a page for signups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get '/contactus', to: 'static_pages#contactus'
-  get  '/about',   to: 'static_pages#about'
+  get  '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
 end
