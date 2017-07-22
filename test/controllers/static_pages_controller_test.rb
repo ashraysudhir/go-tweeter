e7f38911a7e53af66cb0297d3c805a29f8c5d62c
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-22 18:47:28 +0530

    Refactored to remove redundancies

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @title_root = "Go-Tweeter"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Go-Tweeter"
+    assert_select "title", "Home | #{@title_root}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Go-Tweeter"
+    assert_select "title", "Help | #{@title_root}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Go-Tweeter"
+    assert_select "title", "About | #{@title_root}"
   end
 
 end
