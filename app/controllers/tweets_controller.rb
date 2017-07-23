b238fdc87819bc2c9ecfbe142d7272b86897ec3a
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 16:54:44 +0530

    Tweets can now be deleted by its user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   before_action :login_check, only: [:create, :destroy]
+  before_action :user_check, only: :destroy
 
   def create
     @tweet = current_user.tweets.build(tweet_fields)
@@ -14,10 +15,18 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    @tweet.destroy
+    flash[:success] = "Tweet Deleted Successfully!"
+    redirect_to request.referrer || root_url
   end
 
   private
     def tweet_fields
       params.require(:tweet).permit(:content)
+    end
+
+    def user_check
+      @tweet = current_user.tweets.find_by(id: params[:id])
+      redirect_to root_url if @tweet.nil?
     end
 end
