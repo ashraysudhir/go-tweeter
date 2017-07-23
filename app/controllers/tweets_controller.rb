32e6463d1e312a06022079e0c8390423db9ab217
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 16:29:28 +0530

    Tweets can now me composed from home page which displays user information also

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,20 @@ class TweetsController < ApplicationController
   before_action :login_check, only: [:create, :destroy]
 
   def create
+    @tweet = current_user.tweets.build(tweet_fields)
+    if @tweet.save
+      flash[:success] = "Tweeted Successfully!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
 
+  private
+    def tweet_fields
+      params.require(:tweet).permit(:content)
+    end
 end
