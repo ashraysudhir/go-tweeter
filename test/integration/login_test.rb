44eaea0eb36d1c78c20a7eb67030377e58663301
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 11:50:50 +0530

    Refactored the test for successful logout

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -29,16 +29,7 @@ class LoginTest < ActionDispatch::IntegrationTest
   end
 
   test "logout should be successful once login was" do
-    get login_path
-    post login_path, params: { session: { email:    @user.email, password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-    assert_select "a[href=?]", edit_user_path(@user)
+    get user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
