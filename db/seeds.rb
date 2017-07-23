f04c21e36809f66603b57c0572280f03cc5096e5
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 15:17:39 +0530

    Tweets can now be viewed on user profiles in a proper formatted way

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@ User.create!(name:  "Ashray Sudhir", email: "ashraysudhir1@gmail.com", password:
   password = "password"
   User.create!(name:  name, email: email, password: password, password_confirmation: password)
 end
+
+users = User.order(:created_at).take(5)
+50.times do
+  content = Faker::Lorem.sentence(4)
+  users.each { |user| user.tweets.create!(content: content) }
+end
