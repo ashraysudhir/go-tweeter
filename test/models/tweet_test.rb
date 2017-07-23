0ff4cc6693602870ac1679d9ea92b8888390833c
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 14:24:52 +0530

    Tweets created are valid

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:test)
+    @tweet = Tweet.new(content: "Go-Tweet!", user_id: @user.id)
+  end
+
+  test "tweet should be valid" do
+    assert @tweet.valid?
+  end
+
 end
