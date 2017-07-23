e2e91133f47b3a733c4ed89ed3bc058469fdc82f
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 13:17:48 +0530

    Moved the tests of redirection from users_edit_test to users_controller_test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,9 +2,40 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:test)
+    @another_user = users(:test_another)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "edit should be redirected when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "update should be redirected when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "edit should be redirected when logged in as wrong user" do
+    login_as(@another_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "update should be redirected when logged in as wrong user" do
+    login_as(@another_user)
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,7 +4,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:test)
-    @another_user = users(:test_another)
   end
 
   test "invalid edits should be unsuccessful" do
@@ -28,32 +27,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
-  end
-
-  test "edit should be redirected when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "update should be redirected when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "edit should be redirected when logged in as wrong user" do
-    login_as(@another_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "update should be redirected when logged in as wrong user" do
-    login_as(@another_user)
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
   end
 
 end
