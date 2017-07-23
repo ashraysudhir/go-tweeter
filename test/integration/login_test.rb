966e13fb9578716e7b2acd2ea3d0ddc89c99d86a
Ashray Sudhir <ashraysudhir1@gmail.com>
2017-07-23 11:35:15 +0530

    Users can now edit their profile details

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -25,6 +25,7 @@ class LoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 
   test "logout should be successful once login was" do
@@ -37,6 +38,7 @@ class LoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
